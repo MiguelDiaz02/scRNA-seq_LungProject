40b2c4b98c10a6133f781f46494167d4b42940fe
MiguelDiaz02 <g5diazcamposmiguel@gmail.com>
2025-04-21 22:14:28 -0600

    old cb version

--- run_cellbender.sh
+++
@@ -1,2 +0,0 @@
-cellbender remove-background --input /home/mdiaz/hackaton-csbig/ranger/piloto/pre-transplant/tempo/leukimia_gene_raw_bc_matrices.h5ad --output /home/mdiaz/hackaton-csbig/ranger/piloto/clean_adata/cb_leukimia_gene_raw_bc_matrices.h5ad --total-droplets-included 10000 --cuda
-
