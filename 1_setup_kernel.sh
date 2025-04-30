de2d9093d7a12d53203a3174e3852bef107b8672
MiguelDiaz02 <g5diazcamposmiguel@gmail.com>
2025-04-29 19:25:43 -0600

    correct import of libraries

--- 1_setup_kernel.sh
+++ 1_setup_kernel.sh
@@ -4,35 +4,32 @@
 ENV_NAME="theLungPr"
 
 # Verify conda availability
-if ! command -v conda &> /dev/null
-then
-    echo "Conda is not install or is not in PATH, please install Miniconda or Anaconda first."
+if ! command -v conda &> /dev/null; then
+    echo "Conda is not installed or is not in PATH. Please install Miniconda or Anaconda first."
     exit 1
 fi
 
 # Create conda env with Python 3.10 
-echo "Creating Conda: $ENV_NAME"
+echo "Creating Conda environment: $ENV_NAME"
 conda create -y -n $ENV_NAME python=3.10
 
-# Activate the env (we use 'conda run' instead of conda init)
-echo "Installing ipykernel and other packages"
-conda run -n $ENV_NAME pip install ipykernel
-
-pip install \
-scanpy \
-notebook \
-numpy \
-scipy \
-matplotlib \
-pandas \
-scvi-tools \
-seaborn \
-sc_toolbox \
-rpy2 \
-anndata2ri \
-loompy \
-celltypist \
-mygene
+# Install packages INSIDE the environment using conda run
+echo "Installing packages in $ENV_NAME"
+conda run -n $ENV_NAME pip install \
+    scanpy \
+    notebook \
+    numpy \
+    scipy \
+    matplotlib \
+    pandas \
+    scvi-tools \
+    seaborn \
+    sc_toolbox \
+    rpy2 \
+    anndata2ri \
+    loompy \
+    celltypist \
+    mygene
 
 # Add the kernel to Jupyter
 echo "Installing the environment as kernel to Jupyter"
